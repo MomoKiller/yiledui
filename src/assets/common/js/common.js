4ddaea00e52fc23183caa2e1e8c6d451d1080feb
MomoKiller <1226421458@qq.com>
2019-03-28 18:47:15 +0800

    git add src/

--- src/assets/common/js/common.js
+++ src/assets/common/js/common.js
@@ -1,6 +1,6 @@
 export function GetApi(key) {
     const apis = {
-        'searchCategory': '//prize.u[]l[]e.com/mc/couponWall/searchCategory'     // ule网的一个测试接口
+        'searchCategory': '//prize.ule.com/mc/couponWall/searchCategory'     // ule网的一个测试接口
     }
     let apiUrl = apis[key];
     return apiUrl;
